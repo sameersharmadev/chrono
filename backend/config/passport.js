ed0ad87eb53aaf2e7e51db2414d89264c449783f
sameersharmadev <sameerkw77@gmail.com>
2025-07-01 19:47:55 +0530

    UPDATE passport

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -9,7 +9,7 @@ dotenv.config();
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: '/auth/google/callback',
+  callbackURL: process.env.GOOGLE_CALLBACK_URL,
   scope: ['profile', 'email']
 }, async (accessToken, refreshToken, profile, done) => {
   console.log('🔍 Full Google profile:', JSON.stringify(profile, null, 2));
