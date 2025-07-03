15d1482a26df5dfb329169b8d717db67f1a7e9af
Sameer Sharma <sameerkw77@gmail.com>
2025-07-03 08:07:42 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,17 @@ import { startReminderJob } from './jobs/reminderJob.js';
 
 const PORT = process.env.PORT || 5000;
 
+setInterval(() => {
+  fetch('https://chronoapi.sameersharma.me/api/ping')
+    .then(res => res.text())
+    .then(data => console.log('Keep-alive ping response:', data))
+    .catch(err => console.error('Keep-alive ping failed:', err));
+}, 14 * 60 * 1000);
+
+app.get('/api/ping', (req, res) => {
+  res.send('pong');
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
   startReminderJob();
